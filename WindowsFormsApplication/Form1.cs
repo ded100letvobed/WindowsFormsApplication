8e79fa0384ebb6535f962f273729a9b7d3d3f409
User <User@DESKTOP-442GTNQ>
2017-07-23 00:01:03 +0300

    добавил обработчик для кнопки

--- WindowsFormsApplication/Form1.cs
+++ WindowsFormsApplication/Form1.cs
@@ -16,5 +16,15 @@ namespace WindowsFormsApplication
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
